0374081fd74dd30993d8eac814d273cfee0863b0
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-09 21:45:08 +0200

    Added filter functon

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = (props) => {
   return (
-    <div className="pa2">
-      <input className="pa3 ba b--green bg-lightest-blue" type='search' placeholder='search robots' />
+    <div className='pa2'>
+      <input
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
+        onChange={props.searchChange}
+      />
     </div>
   );
 };
