0adfc6daf2b960dda40153db20caee3f917fa9ce
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-10-09 21:17:56 +0200

    Added CardList component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Card from './Card';
-
+import CardList from './CardList';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,28 +10,7 @@ import 'tachyons';
 import { robots } from './robots';
 
 ReactDOM.render(
-  <div>
-    <Card
-      id={robots[0].id}
-      name={robots[0].name}
-      email={robots[0].email}
-    />
-    <Card
-      id={robots[1].id}
-      name={robots[1].name}
-      email={robots[1].email}
-    />
-    <Card
-      id={robots[2].id}
-      name={robots[2].name}
-      email={robots[2].email}
-    />
-    <Card
-      id={robots[3].id}
-      name={robots[3].name}
-      email={robots[3].email}
-    />
-  </div>,
+  <CardList robots={robots} />,
   document.getElementById('root')
 );
 
