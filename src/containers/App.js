92ff56421c373ecf2c7298529771ccc94ad30b02
Katsanakis Georgios <geo.elgeo@gmail.com>
2021-10-16 20:50:52 +0200

    Added redux store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField } from '../actions';
 
 import './App.css';
+
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchField,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) =>
+      dispatch(setSearchField(event.target.value)),
+  };
+};
 
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
       robots: [],
-      searchfield: '',
+      searchField: '',
     };
   }
 
@@ -26,16 +40,13 @@ class App extends Component {
       });
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots} = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((robot) => {
       return robot.name
         .toLowerCase()
-        .includes(searchfield.toLowerCase());
+        .includes(searchField.toLowerCase());
     });
 
     return !robots.length ? (
@@ -43,7 +54,7 @@ class App extends Component {
     ) : (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -54,4 +65,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './containers/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 
 import 'tachyons';
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
